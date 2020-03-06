00917f47dc823be7dd33adb35aba9e5cca5aa74b
marvin lara <laramarvin105@gmail.com>
2020-03-06 18:34:21 -0500

    functional input workout component

--- client/src/components/workout/Workout.js
+++ client/src/components/workout/Workout.js
@@ -3,6 +3,7 @@ import './workout.css';
 import Navbar from '../navbar/Navbar';
 import AddWorkOut from './addWorkOut/AddWorkOut';
 import OutPutWork from './outPutWork/OutPutWork';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import API from '../../utils/API';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -11,15 +12,146 @@ class Workout extends Component {
 		super(props);
 
 		this.state = {
+			workOuts: [],
 			date: '',
 			exercise: '',
 			sets: '',
 			reps: '',
 			weight: '',
 			message: {},
+			id: uuidv4(),
 			editWorkOut: false
 		};
 	}
+
+	handleChangeDate = e => {
+		this.setState({
+			date: e.target.value
+		});
+	};
+
+	handleChangeExercise = e => {
+		this.setState({
+			exercise: e.target.value
+		});
+	};
+
+	handleChangeSets = e => {
+		this.setState({
+			sets: e.target.value
+		});
+	};
+
+	handleChangeReps = e => {
+		this.setState({
+			reps: e.target.value
+		});
+	};
+
+	handleChangeWeight = e => {
+		this.setState({
+			weight: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		const workOutId = {
+			id: this.state.id,
+			dateTitle: this.state.date,
+			exerciseTitle: this.state.exercise,
+			setsTitle: this.state.sets,
+			repsTitle: this.state.reps,
+			weightTitle: this.state.weight
+		};
+
+		const upDatedWorkOut = [...this.state.workOuts, workOutId];
+
+		this.setState({
+			workOuts: upDatedWorkOut,
+			id: uuidv4(),
+			editWorkOut: false
+		});
+	};
+
+	clearWorkOuts = () => {
+		this.setState({
+			workOuts: []
+		});
+	};
+
+	handleDelete = id => {
+		const filterDate = this.state.workOuts.filter(date => date.id !== id);
+
+		const filterExercise = this.state.workOuts.filter(
+			exercise => exercise.id !== id
+		);
+
+		const filterSets = this.state.sets.filter(sets => sets.id !== id);
+
+		const filterReps = this.state.reps.filter(reps => reps.id !== id);
+
+		const filterWeight = this.state.weight.filter(weight => weight.id !== id);
+
+		this.setState({
+			workOuts: {
+				filterDate,
+				filterExercise,
+				filterReps,
+				filterSets,
+				filterWeight
+			}
+		});
+	};
+
+	handleEdit = id => {
+		const filterDate = this.state.workOuts.filter(date => date.id !== id);
+
+		const filterExercise = this.state.workOuts.filter(
+			exercise => exercise.id !== id
+		);
+
+		const filterSets = this.state.sets.filter(sets => sets.id !== id);
+
+		const filterReps = this.state.reps.filter(reps => reps.id !== id);
+
+		const filterWeight = this.state.weight.filter(weight => weight.id !== id);
+
+		this.setState({
+			workOuts: {
+				filterDate,
+				filterExercise,
+				filterReps,
+				filterSets,
+				filterWeight
+			}
+		});
+
+		const selectedDate = this.state.workOuts.find(date => date.id !== id);
+
+		const selectedExercise = this.state.workOuts.find(
+			exercise => exercise.id !== id
+		);
+
+		const selectedSets = this.state.workOuts.find(sets => sets.id !== id);
+
+		const selectedWeight = this.state.workOuts.find(weight => weight.id !== id);
+
+		const selectedReps = this.state.workOuts.find(reps => reps.id !== id);
+
+		this.setState({
+			workOuts: {
+				selectedDate,
+				selectedExercise,
+				selectedReps,
+				selectedSets,
+				selectedWeight
+			},
+			editWorkOut: true,
+			id: id
+		});
+	};
 
 	render() {
 		return (
@@ -29,9 +161,27 @@ class Workout extends Component {
 					<Navbar />
 				</div>
 
-				<div>
-					<AddWorkOut />
-					<OutPutWork />
+				<div className="container">
+					<div className="row">
+						<div className="col-10 mx-auto col-md mt-4">
+							<h4>WorkOut Log</h4>
+							<AddWorkOut
+								date={this.state.date}
+								exercise={this.state.exercise}
+								sets={this.state.sets}
+								reps={this.state.reps}
+								weight={this.state.weight}
+								handleSubmit={this.handleSubmit}
+								editWorkOut={this.state.editWorkOut}
+								handleChangeDate={this.handleChangeDate}
+								handleChangeExercise={this.handleChangeExercise}
+								handleChangeSets={this.handleChangeSets}
+								handleChangeReps={this.handleChangeReps}
+								handleChangeWeight={this.handleChangeWeight}
+							/>
+							<OutPutWork />
+						</div>
+					</div>
 				</div>
 			</div>
 		);
--- client/src/components/workout/addWorkOut/AddWorkOut.js
+++ client/src/components/workout/addWorkOut/AddWorkOut.js
@@ -2,9 +2,80 @@ import React, { Component } from 'react';
 
 export default class AddWorkOut extends Component {
 	render() {
+		const {
+			date,
+			exercise,
+			sets,
+			reps,
+			weight,
+			handleChangeDate,
+			handleChangeExercise,
+			handleChangeSets,
+			handleChangeReps,
+			handleChangeWeight
+			editWorkOut,
+			handleSubmit
+		} = this.props;
 		return (
-			<div>
-				<h3> Add a workout</h3>
+			<div className="card card-body my-3">
+				<form onSubmit={handleSubmit}>
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<div className="input-group-text bg-primary text-white">
+								<i className="fas fa-book"></i>
+							</div>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Date"
+								value={date}
+								onChange={handleChangeDate}
+							/>
+							<br />
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Exercise"
+								value={exercise}
+								onChange={handleChangeExercise}
+							/>
+							<br />
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Sets"
+								value={sets}
+								onChange={handleChangeSets}
+							/>
+							<br />
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Repetitions"
+								value={reps}
+								onChange={handleChangeReps}
+							/>
+							<br />
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Weight"
+								value={weight}
+								onChange={handleChangeWeight}
+							/>
+						</div>
+					</div>
+					<button
+						type="submit"
+						className={
+							editWorkOut
+								? 'btn btn-block btn-success mt-3'
+								: 'btn btn-block btn-primary mt-3'
+						}
+					>
+						{editWorkOut ? 'Edit WorkOut' : 'Add WorkOut'}
+					</button>
+				</form>
 			</div>
 		);
 	}
